03bf67725a0c8f3fce14fd34157d77128672f621
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-11-15 13:46:11 +0600

    middleware implementation done

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,6 +1,13 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:learn_dart_backend/repository/task_repository.dart';
+import 'package:learn_dart_backend/services/task_service.dart';
+
+// Create a singleton instance of the repository
+final _taskRepository = TaskRepository();
+
+// Create a singleton instance of the service, passing the repository to it
+final _taskService = TaskService(_taskRepository);
 
 Handler middleware(Handler handler) {
-  // TODO: implement middleware
-  return handler;
+  return handler.use(provider<TaskService>((context) => _taskService));
 }
